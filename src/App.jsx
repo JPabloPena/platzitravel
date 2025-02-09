086b13b68e96fd7d49c84ffef139dd9295e11404
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-02-09 12:40:13 -0500

    chore: initialize project with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-96 h-96 bg-gray-100 p-10 m-20 flex space-x-4 justify-end items-end'>
+        <div className='w-16 h-16 bg-red-300'></div>
+        <div className='w-16 h-16 bg-red-300'></div>
+        <div className='w-16 h-16 bg-red-300'></div>
+        <div className='w-16 h-16 bg-red-300'></div>
+      </div>
+    </>
+  )
+}
+
+export default App
