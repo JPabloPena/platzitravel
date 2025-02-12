76543f7709680c47a68faa8be2b760eeebbc408a
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-02-12 18:46:34 -0500

    feat: add transitions and lint code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function Home() {
+function Home () {
   return (
-    <div id='home'>
+    <section id='home'>
       <div className='w-full h-3/4'>
         <div className='w-full h-full'>
-          <img src='/images/sanFrancisco.jpg'/>
+          <img src='/images/sanFrancisco.jpg' />
           <div className='w-full h-full flex flex-col absolute space-y-96 py-4 items-center top-0'>
-            <input className='bg-white outline-none p-3 rounded-full shadow-sm text-center' type='search' placeholder='San Francisco' />
-            <button className='w-36 bg-white text-xl font-semibold p-4 rounded-full text-primary shadow-sm'>Explore</button>
+            <input className='bg-white outline-none p-3 rounded-full shadow-sm text-center transition duration-300 focus-within:shadow-sm focus:ring-2 focus:w-11/12' type='search' placeholder='San Francisco' />
+            <button className='w-48 bg-white text-lg font-semibold p-4 rounded-full text-primary shadow-sm transition duration-500 ease-in-out hover:bg-primary hover:text-white hover:translate-y-1 hover:scale-110 cursor-pointer'>Explore</button>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
