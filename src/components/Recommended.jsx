f99b5aac4e38b725308f65457fc331f9d3e75c75
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-02-17 21:24:42 -0500

    refactor: move CARDS data to src/constants

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,33 +1,6 @@
 import React from 'react'
 import RecommendedCard from './RecommendedCard'
-
-const CARDS = [
-  {
-    imageClass: 'bg-norway',
-    title: 'Norway',
-    desc: 'Beautiful landscapes'
-  },
-  {
-    imageClass: 'bg-newYork',
-    title: 'New York',
-    desc: 'Concrete jungle'
-  },
-  {
-    imageClass: 'bg-yosemite',
-    title: 'Yosemite',
-    desc: 'A break from the world'
-  },
-  {
-    imageClass: 'bg-seattle',
-    title: 'Seattle',
-    desc: 'Big city'
-  },
-  {
-    imageClass: 'bg-switzerland',
-    title: 'Switzerland',
-    desc: 'Big city'
-  }
-]
+import CARDS from '../constants/recommendedCards'
 
 function Recommended () {
   return (
---
+++ src/constants/recommendedCards.js
@@ -0,0 +1,29 @@
+const CARDS = [
+  {
+    imageClass: 'bg-norway',
+    title: 'Norway',
+    desc: 'Beautiful landscapes'
+  },
+  {
+    imageClass: 'bg-newYork',
+    title: 'New York',
+    desc: 'Concrete jungle'
+  },
+  {
+    imageClass: 'bg-yosemite',
+    title: 'Yosemite',
+    desc: 'A break from the world'
+  },
+  {
+    imageClass: 'bg-seattle',
+    title: 'Seattle',
+    desc: 'Big city'
+  },
+  {
+    imageClass: 'bg-switzerland',
+    title: 'Switzerland',
+    desc: 'Big city'
+  }
+]
+
+export default CARDS
