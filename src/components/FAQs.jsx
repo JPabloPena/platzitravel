d60a0e763623a0c0e3f3425d81af79e87bc8d2a9
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-02-17 21:24:18 -0500

    refactor: move FAQS data to src/constants

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,28 +1,6 @@
 import React from 'react'
 import FAQsCard from './FAQsCard'
-
-const FAQS = [
-  {
-    title: 'Cancellation Policy',
-    desc: 'For stays of less than one week it is important to give 3 days notice, otherwise you will be sanctioned.'
-  },
-  {
-    title: 'Methods of Payment',
-    desc: 'We accept different payment methods: VISA, MasterCard, American Express, Paypal and Binance.'
-  },
-  {
-    title: 'Pets',
-    desc: 'The issue of pets will depend directly on the host.'
-  },
-  {
-    title: 'Security Information',
-    desc: 'All our stays are insured in case of accidents.'
-  },
-  {
-    title: 'Long Stays',
-    desc: 'Stays of up to 3 months are available, in this case a deposit of 50% of the rental value is required.'
-  }
-]
+import FAQS from '../constants/faqs'
 
 function FAQs () {
   return (
