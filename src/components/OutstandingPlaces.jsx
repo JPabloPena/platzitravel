109dde3595ca3466b838756466acc1fc489fa22c
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-02-17 21:24:29 -0500

    refactor: move CARDS data to src/constants

--- src/components/OutstandingPlaces.jsx
+++ src/components/OutstandingPlaces.jsx
@@ -1,30 +1,6 @@
 import React from 'react'
 import OutstandingPlacesCard from './OutstandingPlacesCard'
-
-const CARDS = [
-  {
-    imageClass: 'bg-chicago',
-    title: 'Chicago',
-    desc: '2 rooms, bathroom and kitchen.',
-    customClass: 'lg:col-span-2'
-  },
-  {
-    imageClass: 'bg-LA',
-    title: 'Los Angeles',
-    desc: '4 rooms, 3 bathrooms, kitchen and private pool.',
-    customClass: 'lg:row-span-2 lg:h-full'
-  },
-  {
-    imageClass: 'bg-miami',
-    title: 'Miami',
-    desc: '3 rooms, 2 bathrooms, kitchen and amazing sea view.'
-  },
-  {
-    imageClass: 'bg-bali',
-    title: 'Bali',
-    desc: '2 rooms, 2 bathrooms, kitchen and private pool.'
-  }
-]
+import CARDS from '../constants/outstandingPlacesCards'
 
 function OutstandingPlaces () {
   return (
---
+++ src/constants/outstandingPlacesCards.js
@@ -0,0 +1,26 @@
+const CARDS = [
+  {
+    imageClass: 'bg-chicago',
+    title: 'Chicago',
+    desc: '2 rooms, bathroom and kitchen.',
+    customClass: 'lg:col-span-2'
+  },
+  {
+    imageClass: 'bg-LA',
+    title: 'Los Angeles',
+    desc: '4 rooms, 3 bathrooms, kitchen and private pool.',
+    customClass: 'lg:row-span-2 lg:h-full'
+  },
+  {
+    imageClass: 'bg-miami',
+    title: 'Miami',
+    desc: '3 rooms, 2 bathrooms, kitchen and amazing sea view.'
+  },
+  {
+    imageClass: 'bg-bali',
+    title: 'Bali',
+    desc: '2 rooms, 2 bathrooms, kitchen and private pool.'
+  }
+]
+
+export default CARDS
